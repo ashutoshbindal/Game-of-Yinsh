9aba4bb1a6efa4a2695040035e40d73f8af70ba4
Ashutosh Bindal <ashutoshbindal@gmail.com>
2018-09-13 18:55:32 +0530

    -

--- main.cpp
+++ main.cpp
@@ -14,34 +14,34 @@ int main(int argc, char** argv) {
     initialize();
 
     // Get input from server about game specifications
-    // cin >> player_id >> board_size >> time_limit;
-    //
-    // if(player_id == 2) {
-    //     // Get other player's move
-    //     cin>>move;
-    //     update_opponent(move);
-    //
-    //     while(true) {
-    //     	move = get_move();
-    //     	update_self(move);
-    //         cout<<move<<endl;
-    //         cin>>move;
-    //         update_opponent(move);
-    //     }
-    // }
-    // else if(player_id == 1) {
-    //     while(true) {
-    //     	move = get_move();
-    //     	update_self(move);
-    //         cout<<move<<endl;
-    //         cin>>move;
-    //         update_opponent(move);
-    //     }
-    // }
+    cin >> player_id >> board_size >> time_limit;
+
+    if(player_id == 2) {
+        // Get other player's move
+        cin>>move;
+        update_opponent(move);
+
+        while(true) {
+        	move = get_move();
+        	update_self(move);
+            cout<<move<<endl;
+            cin>>move;
+            update_opponent(move);
+        }
+    }
+    else if(player_id == 1) {
+        while(true) {
+        	move = get_move();
+        	update_self(move);
+            cout<<move<<endl;
+            cin>>move;
+            update_opponent(move);
+        }
+    }
 
     //testing the conversion data structure
-    pair<int, int> var_temp = {0, 0};
-    cordinate2 temp = hex2cart[var_temp];
-    cout<<temp.x<<" "<<temp.y<<" "<<temp.z<<endl;
+    // pair<int, int> var_temp = {0, 0};
+    // cordinate2 temp = hex2cart[var_temp];
+    // cout<<temp.x<<" "<<temp.y<<" "<<temp.z<<endl;
     return 0;
 }
